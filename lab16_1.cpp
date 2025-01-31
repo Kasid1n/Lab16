558ce80b2136f65032dbcedc5c151345759c2ef9
kasidin <kasidinkhamfoo@gmail.com>
2025-01-31 17:46:11 +0700

    Lab16_1.cpp completed

--- lab16_1.cpp
+++ lab16_1.cpp
@@ -32,3 +32,34 @@ int main(){
 	findColSum(dPtr,sum2,N,M); 
 	showData(sum2,1,M);
 }
+void findRowSum(const double* x, double* y,int n,int m){
+   for(int i = 0; i < n; i++){
+        *(y+i)=0;
+        for(int j=0;j<m;j++){
+            *(y+i)+=*(x+(i*m)+j);
+            }
+       
+   }
+}
+void findColSum(const double* x, double* y,int n,int m){
+   for(int i = 0; i < m; i++){
+        *(y+i)=0;
+        for(int j=0;j<n;j++){
+            *(y+i)+=*(x+(i)+(j*m));
+            }
+       
+   }
+}
+void randData(double *x,int N,int M){
+    for(int i = 0; i < N*M; i++){
+        *(x+i) = (rand()%101)*0.01;
+    }
+}
+void showData(double *x,int N, int M){
+    cout << fixed << setprecision(2);
+    for(int i = 0; i < N*M; i++){
+        cout << *(x+i);
+        if((i+1)%M==0||M==1) cout << endl;
+        else cout << " ";
+    }
+}
